e0aef943350976dd814bfcd8851326af4f9438e9
DAMIAN M <12damie@gmail.com>
2020-04-28 10:44:22 +0200

    Big update

--- app/src/main/java/com/example/lab7/MainActivity.java
+++ app/src/main/java/com/example/lab7/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -26,24 +28,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Atribution items to variable
         Button button=findViewById(R.id.button);
         editText=findViewById(R.id.editText1);
+
+        //Load save date
         SharedPreferences sharedPreferences=getSharedPreferences("shared",MODE_PRIVATE);
         String data=sharedPreferences.getString("data","");
+
         editText.setText(data);
+
+        //Service click in button
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 String city=editText.getText().toString();
+
                 Intent intent=new Intent(MainActivity.this, WeatherActivity.class);
+                //Transmission date
                 intent.putExtra("City",city);
+                //Save date
                 saveData(city);
+                //Start activity
                 startActivity(intent);
+
             }
         });
     }
-
+    //Method save date
     private void saveData(String city) {
         SharedPreferences sharedPreferences=getSharedPreferences("shared",MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
--- app/src/main/java/com/example/lab7/WeatherActivity.java
+++ app/src/main/java/com/example/lab7/WeatherActivity.java
@@ -2,13 +2,21 @@ package com.example.lab7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,21 +26,26 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class WeatherActivity extends AppCompatActivity implements Runnable{
 
+    private String zoneT=new String("");
     private TextView textView;
+    private ImageView imageView;
     private TextView textTime;
     private TextView textTemp;
     private TextView textPressure;
     private TextView textHumidity;
     private TextView textTempMin;
     private TextView textTempMax;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        //Receive date
         Intent intent=getIntent();
         String city=intent.getStringExtra("City");
-        city+=",pl";
+
+        //Atribution items to variable
         textView=findViewById(R.id.name_city);
         textTime=findViewById(R.id.time);
         textTemp=findViewById(R.id.temp);
@@ -40,15 +53,22 @@ public class WeatherActivity extends AppCompatActivity implements Runnable{
         textHumidity=findViewById(R.id.humidity);
         textTempMin=findViewById(R.id.tempMin);
         textTempMax=findViewById(R.id.tempMax);
-        start();
+        imageView=findViewById(R.id.imageView2);
+
+        //Strat Method
         show(city);
+        start();
+
     }
 
+    //Method show time  witch timezZone include a JSON information
     public void showTime(){
-        textTime.setText(getTime());
+        textTime.setText(getTime(TimeZone.getTimeZone(zoneT)));
     }
 
+    //Method show information about city
     public void show(final String city){
+
 
         Retrofit retrofit=new Retrofit.Builder()
                 .baseUrl("http://api.openweathermap.org/")
@@ -56,25 +76,38 @@ public class WeatherActivity extends AppCompatActivity implements Runnable{
                 .build();
 
         JsonPlaceholderAPI jsonPlaceholderAPI=retrofit.create(JsonPlaceholderAPI.class);
+        //Set Query
         Call call=jsonPlaceholderAPI.getCurrentWeatherData(city,"metric","749561a315b14523a8f5f1ef95e45864");
 
         call.enqueue(new Callback() {
+            //Receive date
             @Override
             public void onResponse(Call call, Response response) {
-                if(!response.isSuccessful()){
-                    finish();
+                //Catch error Query link
+               if(response.code()==404) {
+                   textView.setText("Zle dane");
+                   finish();
+                   return;
+               }
+               //Show erro code witch Query http
+                else if(!response.isSuccessful()){
                     textView.setText("Code"+response.code());
+                    finish();
                     return;
                 }
+                //Show if Query is success
                 PostWeather postWeathers= (PostWeather) response.body();
+                checkZoneTime(postWeathers.getTimezone());
                 textView.setText(postWeathers.getName());
                 textTemp.setText(Html.fromHtml(Float.toString(postWeathers.getMain().getTemp())+" <sup>o</sup>C"));
                 textPressure.setText(Float.toString(postWeathers.getMain().getPressure())+" hPa");
                 textHumidity.setText(Float.toString(postWeathers.getMain().getHumidity())+" %");
                 textTempMin.setText(Html.fromHtml(Float.toString(postWeathers.getMain().getTemp_min())+" <sup>o</sup>C"));
                 textTempMax.setText(Html.fromHtml(Float.toString(postWeathers.getMain().getTemp_max())+" <sup>o</sup>C"));
+                //Show picture witch url link
+                Picasso.get().load("http://openweathermap.org/img/wn/"+postWeathers.getWeather().get(0).getIcon()+".png").into(imageView);
             }
-
+            //Show error
             @Override
             public void onFailure(Call call, Throwable t) {
                 textView.setText(t.getMessage());
@@ -82,19 +115,34 @@ public class WeatherActivity extends AppCompatActivity implements Runnable{
         });
 
     }
-    public String getTime(){
-        SimpleDateFormat formatter= new SimpleDateFormat("HH:mm:ss");
-        Date date = new Date(System.currentTimeMillis());
-        String time=formatter.format(date);
-        return  time;
+    //Method convert TimeZone witch date JSON
+    private void checkZoneTime(float Time) {
+        int zone=(int)Time/3600;
+        zoneT+="GMT";
+        if(zone>0){
+                zoneT+="+";
+                zoneT+=Integer.toString(zone);
+            }
+            else{
+                zoneT+=Integer.toString(zone);
+            }
     }
 
+    //Method get time for TimzeZone
+    public String getTime(TimeZone time){
+        Date date = new Date();
+        DateFormat requiredFormat = new SimpleDateFormat("HH:mm:ss ");
+        requiredFormat.setTimeZone(time);
+        String strCurrentDay = requiredFormat.format(date).toUpperCase();
+        return strCurrentDay;
+    }
 
+    //Method create and start Thread
     public void start() {
         Thread thread = new Thread(this);
         thread.start();
     }
-
+    //Method service Thread
     public void run() {
         while (true) {
             showTime();
