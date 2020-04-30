c08cb1e1e0eacd1d5bba3316a1a39c23029174fa
DAMIAN M <12damie@gmail.com>
2020-04-30 21:37:58 +0200

    Big update

---
+++ app/src/main/java/com/example/lab7/Wind.java
@@ -0,0 +1,19 @@
+package com.example.lab7;
+
+class Wind {
+    public float speed;
+    public float deg;
+    public float gust;
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public float getDeg() {
+        return deg;
+    }
+
+    public float getGust() {
+        return gust;
+    }
+}
